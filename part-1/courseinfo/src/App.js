e6761a281320d79a73ccd060b3e115b89cd2b79f
Danny Ondrisek <technikal85@gmail.com>
2023-04-17 09:14:35 -0500

    Refactored Content component into sub components

--- part-1/courseinfo/src/App.js
+++ part-1/courseinfo/src/App.js
@@ -10,9 +10,14 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part2} exercises2={exercises2} />
-      <Content part3={part3} exercises3={exercises3} />
+      <Content
+        Part1 part1={part1}
+        exercises1={exercises1} 
+        Part2 part2={part2}
+        exercises2={exercises2}
+        Part3 part3={part3}
+        exercises3={exercises3}
+      />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
@@ -31,14 +36,44 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  return (
+  <>
+  <Part1 part1={part1} exercises1={exercises1} />
+  <Part2 part2={part2} exercises2={exercises2} />
+  <Part3 part3={part3} exercises3={exercises3}/>
+  </>
+  )
+};
+
+const Part1 = (props) => {
   return (
     <div>
       <p>
         {props.part1} {props.exercises1}
       </p>
+    </div>
+  );
+};
+
+const Part2 = (props) => {
+  return (
+    <div>
       <p>
         {props.part2} {props.exercises2}
       </p>
+    </div>
+  );
+};
+
+const Part3 = (props) => {
+  return (
+    <div>
       <p>
         {props.part3} {props.exercises3}
       </p>
@@ -56,6 +91,5 @@ const Total = (props) => {
     </div>
   );
 };
-
 
 export default App;
